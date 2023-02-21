0391267922675d6bc54dce7fe5884cbec6b63aae
Sviatoslav Chapcha <whitetark@gmail.com>
2023-02-21 18:58:47 +0200

    better theme picker for social media dashboard

--- practice/front-end-mentor/03.social-media-dashboard-with-theme-switcher/scripts.js
+++ practice/front-end-mentor/03.social-media-dashboard-with-theme-switcher/scripts.js
@@ -1,21 +1,24 @@
-function darkMode() {
-  if (document.documentElement.style.getPropertyValue('--theme') == 'dark-theme') {
-    document.documentElement.style.setProperty('--toggle', 'hsl(233, 19%, 91%)')
-    document.documentElement.style.setProperty('--bg', 'hsl(0, 0%, 100%)')
-    document.documentElement.style.setProperty('--top-bg-pattern', 'hsl(225, 100%, 98%)')
-    document.documentElement.style.setProperty('--card-bg', 'hsl(227, 47%, 96%)')
-    document.documentElement.style.setProperty('--text-light', 'hsl(228, 12%, 44%)')
-    document.documentElement.style.setProperty('--text-dark', 'hsl(230, 17%, 14%)')
-    document.documentElement.style.setProperty('--theme', 'light-theme')
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName)
+  document.documentElement.className = themeName
+}
+
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-light')
     document.getElementById('btn').innerHTML = 'Off'
   } else {
-    document.documentElement.style.setProperty('--toggle', 'hsl(229, 25%, 14%)')
-    document.documentElement.style.setProperty('--bg', 'hsl(230, 17%, 14%)')
-    document.documentElement.style.setProperty('--top-bg-pattern', 'hsl(232, 19%, 15%)')
-    document.documentElement.style.setProperty('--card-bg', 'hsl(228, 28%, 20%)')
-    document.documentElement.style.setProperty('--text-light', 'hsl(228, 34%, 66%)')
-    document.documentElement.style.setProperty('--text-dark', 'hsl(0, 0%, 100%)')
-    document.documentElement.style.setProperty('--theme', 'dark-theme')
+    setTheme('theme-dark')
     document.getElementById('btn').innerHTML = 'On'
   }
 }
+
+;(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark')
+    document.getElementById('btn').innerHTML = 'On'
+  } else {
+    setTheme('theme-light')
+    document.getElementById('btn').innerHTML = 'Off'
+  }
+})()
