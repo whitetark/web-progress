66749bddcdabe15c08ebb9075425ce1cb2d1157d
Sviatoslav Chapcha <whitetark@gmail.com>
2023-08-29 15:30:44 +0300

    auth system

---
+++ practice/react-learning/14.auth-system/frontend/src/components/AuthForm.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { Form, Link, useActionData, useSearchParams } from 'react-router-dom'
+
+import classes from './AuthForm.module.css'
+
+function AuthForm() {
+  const data = useActionData()
+
+  const [searchParams] = useSearchParams()
+  const isLogin = searchParams.get('mode') === 'login'
+
+  return (
+    <>
+      <Form method='post' className={classes.form}>
+        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.values).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
+        <p>
+          <label htmlFor='email'>Email</label>
+          <input id='email' type='email' name='email' required />
+        </p>
+        <p>
+          <label htmlFor='password'>Password</label>
+          <input id='password' type='password' name='password' required />
+        </p>
+        <div className={classes.actions}>
+          <Link to={`?mode=${isLogin ? 'singup' : 'login'}`}>
+            {isLogin ? 'Create new user' : 'Login'}
+          </Link>
+          <button type='submit'>Save</button>
+        </div>
+      </Form>
+    </>
+  )
+}
+
+export default AuthForm
